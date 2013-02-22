1ed6e959925d8404f43662230c2f805766b02712
Ted Han <ted@knowtheory.net>
2013-02-22 07:56:53 -0600

    Comments!

--- java/src/org/documentcloud/pdftailor/PdfTailor.java
+++ java/src/org/documentcloud/pdftailor/PdfTailor.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Iterator;
- 
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfCopy;
@@ -20,32 +20,58 @@ import com.beust.jcommander.MissingCommandException;
 
 public class PdfTailor {
   
-  // Class Runner
   public static void main( String[] args ) throws IOException, DocumentException {
+
+    // Initialize the JCommander parsers
+    JCommander cli = new JCommander();
     StitchCommand   stitch   = new StitchCommand();
     UnstitchCommand unstitch = new UnstitchCommand();
-    JCommander cli = new JCommander();
-    
+
+    // Add our Stitch and Unstitch commands to the parser
     cli.addCommand("stitch",   stitch);
     cli.addCommand("unstitch", unstitch);
 
+    // When called with no arguments display the usage information.
     if (args.length == 0) { 
       usage(); 
     } else {
       try { 
+        // Parse the provided arguments.
         cli.parse(args);
-
         String command = cli.getParsedCommand();
         if      (command.equals("stitch"))   { stitch(stitch); } 
         else if (command.equals("unstitch")) { unstitch(unstitch); }
         else                                 { usage(); }
         
       } catch (MissingCommandException unrecognizedCommand) {
+        // and if unrecognized, display the usage information.
         usage();
       }
     }
   }
 
+  public static String VERSION       = "0.0.1";
+  public static String USAGE_MESSAGE = "pdftailor stitches and unstitches pdfs.\n\n"                        +
+                                       "Version: " + VERSION + "\n\n"                                       +
+                                       "Usage:\n"                                                           +
+                                       "  pdftailor COMMAND [OPTIONS] <pdf(s)>\n"                           +
+                                       "  Main commands:\n"                                                 +
+                                       "    stitch, unstitch\n\n"                                           +
+                                       "Options:\n"                                                         +
+                                       "  -o, --output\n"                                                   +
+                                       "    The file name or file pattern to which output is written.\n"    +
+                                       "    For commands like unstitch which will write multiple files\n"   +
+                                       "    a pattern including \"%d\" can be used to specify a template\n" +
+                                       "    for where files should be written (e.g. ./foo/bar_%d.pdf).\n\n" +
+                                       "Example:\n"                                                         +
+                                       "  pdftailor stitch --output merged.pdf a.pdf b.pdf\n"               +
+                                       "  pdftailor unstitch --output merged_page_%d.pdf merged.pdf\n";
+
+  public static void usage() {
+   System.out.println(USAGE_MESSAGE);
+  }
+
+  // The stitch command parser.
   static class StitchCommand {
     @Parameter(description = "The list of files to stitch together.")
     private List<String> files;
@@ -60,7 +86,10 @@ public class PdfTailor {
     Document document = new Document();
     PdfCopy writer = new PdfCopy(document, new FileOutputStream(outputName));
     document.open();
-    
+
+    // Loop over all of the pdfs specified on the commandline
+    // read out their contents and concatenate them
+    // in the order specified
     PdfReader reader;
     Iterator<String> pdfPaths = cli.files.iterator();
     while (pdfPaths.hasNext()) {
@@ -69,11 +98,14 @@ public class PdfTailor {
       for ( int pageNumber = 0; pageNumber < reader.getNumberOfPages(); ) {
         writer.addPage(writer.getImportedPage( reader, ++pageNumber ));
       }
+      // make sure to free the reader to prevent memory leak
+      // especially for very large PDFs.
       writer.freeReader(reader);
     }
     document.close();
   }
 
+  // The unstitch command parser.
   static class UnstitchCommand {
     @Parameter(description = "The file to unstitch.")
     private List<String> files;
@@ -83,10 +115,14 @@ public class PdfTailor {
   }
 
   public static void unstitch( UnstitchCommand cli ) throws IOException, DocumentException {
+    // use JCommander's default file list to get the file to split.
     String readerPath = cli.files.get(0);
     PdfReader reader = new PdfReader(readerPath);
+    
+    // Loop over the document's pages by page number.
     for ( int pageNumber = 0; pageNumber < reader.getNumberOfPages(); ) {
       pageNumber++;
+
       Document document = new Document();
       PdfCopy writer = new PdfCopy(document, new FileOutputStream(outputPath(cli.output, pageNumber)));
 
@@ -97,37 +133,25 @@ public class PdfTailor {
     }
   }
 
+  // Return a file path for page when provided with a page number and
+  // either an existing file path, or a path template.
   protected static String outputPath( String outputName, int pageNumber ) {
     Pattern templatePattern = Pattern.compile("%d");
     Pattern filePattern = Pattern.compile("^(.+)\\.pdf$");
-    
     String path;
+
+    // if outputName contains %d, we assume it's a template.
     if ( templatePattern.matcher(outputName).find() ){
+      // replace %d with the page number.
       path = String.format(outputName, pageNumber);
     } else if ( filePattern.matcher(outputName).find() ) {
+    // if outputName is a pdf, chop off the file ending and insert the page number before reattaching.
       path = filePattern.matcher(outputName).replaceFirst("$1_" + pageNumber + ".pdf");
     } else {
+      // otherwise we'll just append the page number and add the pdf file extension.
       path = outputName + "_" + pageNumber + ".pdf";
     }
     return path;
   }
 
-  public static void usage() {
-    System.out.println(USAGE_MESSAGE);
-  }
-
-  public static String USAGE_MESSAGE = "pdftailor stitches and unstitches pdfs.\n\n"                        +
-                                       "Usage:\n"                                                           +
-                                       "  pdftailor COMMAND [OPTIONS] <pdf(s)>\n"                           +
-                                       "  Main commands:\n"                                                 +
-                                       "    stitch, unstitch\n\n"                                           +
-                                       "Options:\n"                                                         +
-                                       "  -o, --output\n"                                                   +
-                                       "    The file name or file pattern to which output is written.\n"    +
-                                       "    For commands like unstitch which will write multiple files\n"   +
-                                       "    a pattern including \"%d\" can be used to specify a template\n" +
-                                       "    for where files should be written (e.g. ./foo/bar_%d.pdf).\n\n" +
-                                       "Example:\n"                                                         +
-                                       "  pdftailor stitch --output merged.pdf a.pdf b.pdf\n"               +
-                                       "  pdftailor unstitch --output merged_page_%d.pdf merged.pdf\n";
 }
